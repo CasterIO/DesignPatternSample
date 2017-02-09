1937270932d167b87c1e5803d342ae7fe993a3c4
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-02-09 16:49:39 +0100

    Creates another test to show the use of the builder paradigm in different ways

--- builder/src/test/java/it/tiwiz/builder/VeryComplexObjectTest.java
+++ builder/src/test/java/it/tiwiz/builder/VeryComplexObjectTest.java
@@ -35,4 +35,19 @@ public class VeryComplexObjectTest {
         Assert.assertEquals(THIRD, obj.getThird());
         Assert.assertEquals(FOURTH, obj.getFourth());
     }
+
+    @Test
+    public void testBuilderCreatesCorrectlyTheObjectWithNoFluidApiUsage() throws Exception {
+        VeryComplexObject.Builder builder = new VeryComplexObject.Builder();
+        builder.setSecond(SECOND);
+        builder.setFourth(FOURTH);
+        builder.setFirst(FIRST);
+        builder.setThird(THIRD);
+        VeryComplexObject obj = builder.build();
+
+        Assert.assertEquals(FIRST, obj.getFirst());
+        Assert.assertEquals(SECOND, obj.getSecond());
+        Assert.assertEquals(THIRD, obj.getThird());
+        Assert.assertEquals(FOURTH, obj.getFourth());
+    }
 }
