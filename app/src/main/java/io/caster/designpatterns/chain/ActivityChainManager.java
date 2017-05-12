183570d67ee8750c53315a1e600e6cd05ab974db
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-05-12 15:50:30 +0200

    Add Adapter pattern

--- app/src/main/java/io/caster/designpatterns/chain/ActivityChainManager.java
+++ app/src/main/java/io/caster/designpatterns/chain/ActivityChainManager.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
+import io.caster.designpatterns.adapter.AdapterActivity;
 import io.caster.designpatterns.builder.BuilderActivity;
 import io.caster.designpatterns.decorator.DecoratorActivity;
 import io.caster.designpatterns.observer.ObserverActivity;
@@ -17,10 +18,12 @@ public class ActivityChainManager {
         ActivityChainElement chainOfResponsibility = new ActivityChainElement(1, new Intent(host, ChainOfResponsibilityActivity.class), host);
         ActivityChainElement builder = new ActivityChainElement(2, new Intent(host, BuilderActivity.class), host);
         ActivityChainElement decorator = new ActivityChainElement(3, new Intent(host, DecoratorActivity.class), host);
+        ActivityChainElement adapter = new ActivityChainElement(4, new Intent(host, AdapterActivity.class), host);
 
         head.setNext(chainOfResponsibility);
         chainOfResponsibility.setNext(builder);
         builder.setNext(decorator);
+        decorator.setNext(adapter);
     }
 
     public void onPatternReceived(int patternId) {
