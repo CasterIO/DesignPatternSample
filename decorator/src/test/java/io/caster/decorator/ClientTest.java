c59500212ad6f7ace8ceaa41479ed5ff6f81f38a
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-10-12 22:12:39 +0200

    Add test cases

---
+++ decorator/src/test/java/io/caster/decorator/ClientTest.java
@@ -0,0 +1,50 @@
+package io.caster.decorator;
+
+import org.junit.Test;
+
+import static io.caster.decorator.Prices.ALMOND_MILK;
+import static io.caster.decorator.Prices.ARABIC_COFFEE;
+import static io.caster.decorator.Prices.CARAMEL;
+import static io.caster.decorator.Prices.CHOCOLATE;
+import static io.caster.decorator.Prices.COFFEE;
+import static io.caster.decorator.Size.M;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ClientTest {
+
+    @Test
+    public void first_client() throws Exception {
+        Beverage test = new Coffee(M);
+        assertThat(test.getPrice())
+                .isEqualTo(COFFEE.withSize(M));
+    }
+
+    @Test
+    public void second_client() throws Exception {
+        Beverage test = new AlmondMilk(new ArabicCoffee(M));
+        float expectedPrice = ALMOND_MILK.getValue() + ARABIC_COFFEE.withSize(M);
+        assertThat(test.getPrice())
+                .isEqualTo(expectedPrice);
+    }
+
+    @Test
+    public void third_client() throws Exception {
+        Beverage test = new Chocolate(
+                new Caramel(
+                        new AlmondMilk(
+                                new Coffee(M)
+                        )
+                )
+        );
+
+        float expectedPrice = CHOCOLATE.getValue() +
+                CARAMEL.getValue() + ALMOND_MILK.getValue() +
+                COFFEE.withSize(M);
+
+        assertThat(test.getPrice())
+                .isEqualTo(expectedPrice);
+    }
+
+
+}
